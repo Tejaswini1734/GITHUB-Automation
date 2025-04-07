c4bdae09ab1d9e423c8571c8fbd8bc4508bb84c5
Tejaswini1734 <Tejaswini.v317gmail.com>
2025-04-07 10:31:08 +0530

    initial commit

---
+++ solutions/Solution.java
@@ -0,0 +1,26 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+package solutions;
+
+import java.util.HashSet;
+
+public class Solution {
+   public Solution() {
+   }
+
+   public static boolean containsDuplicate(int[] var0) {
+      HashSet var1 = new HashSet();
+      int[] var2 = var0;
+      int var3 = var0.length;
+
+      for(int var4 = 0; var4 < var3; ++var4) {
+         int var5 = var2[var4];
+         if (var1.contains(var5)) {
+            return true;
+         }
+
+         var1.add(var5);
+      }
+
+      return false;
+   }
+}
